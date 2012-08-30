fa1fc13632e4da434d9175b36aa314faca1c35ea
Andre Chalom <andrechalom@gmail.com>
2012-08-30 18:47:55 +0000

    Corretor com alternativas

--- corretor.R
+++ corretor.R
@@ -1,12 +1,19 @@
+# Conexao com o banco de dados funciona no servidor da lage:
+#library(RMySQL)
+#con <-dbConnect(MySQL(), user="notaR", password="autonota", dbname="notaR")
 
 file.exerc <- "exerc_3_ma.r"
-
 file.test <- "respostas_3.r"
 
-corrige <- function (file.exerc, file.test) {
+#Corrige simplesmente faz source() em 3 arquivos
+corrige <- function (file.prev, file.exerc, file.test) {
+		if (!is.null(file.prev)) source(file.prev)
 		source(file.exerc)
 		source(file.test)
 		return(sum(ANSWERS))
 }
 
-print(corrige(file.exerc, file.test))
+print(corrige(NULL, file.exerc, file.test))
+
+# Alternativa: parse+eval:
+eval(parse(text="y<-rnorm(5)"))
