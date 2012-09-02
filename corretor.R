4af4f52af3b473149681bc834e6980adc3ecb47c
Andre Chalom <andrechalom@gmail.com>
2012-09-02 14:07:07 +0000

    Versao final do corretoR, mudancas na documentacao

--- corretor.R
+++ corretor.R
@@ -1,19 +1,150 @@
-# Conexao com o banco de dados funciona no servidor da lage:
-#library(RMySQL)
-#con <-dbConnect(MySQL(), user="notaR", password="autonota", dbname="notaR")
+# Usa uma conexao "global" con
+connect <- function () {
+		require(RMySQL)
+		# Conexao com o banco de dados
+		try(dbDisconnect(con), silent=TRUE)
+		con<- dbConnect(MySQL(), user="notaR", password="autonota", dbname="notaR")
+		return (con);
+}
+con <- connect()
 
-file.exerc <- "exerc_3_ma.r"
-file.test <- "respostas_3.r"
+# corretoR recebe: 
+# numero.aula, numero.exercicio
+# texto 
+# E devolve um um vector logico com o resultado dos testes
+# Caso o codigo tenha erros de sintaxe, retorna NULL
+corretoR <- function (numero.aula, numero.exercicio, texto) {
+		# Definicoes iniciais
+		corrEnv <- new.env()
+		id.exerc <- dbGetQuery(con, 
+							   paste("SELECT id_exercicio FROM exercicio 
+									 WHERE numero_aula=", numero.aula,
+									 " AND numero_exercicio=",numero.exercicio, sep=""));
+		testes <- dbGetQuery(con,
+							 paste("SELECT condicao FROM teste
+								   WHERE id_exercicio=", id.exerc,
+								   " ORDER BY ordem ASC", sep=""));
+		precondi <- dbGetQuery(con, 
+							   paste("SELECT precondicoes FROM exercicio 
+									 WHERE id_exercicio=", id.exerc, sep=""));
 
-#Corrige simplesmente faz source() em 3 arquivos
-corrige <- function (file.prev, file.exerc, file.test) {
-		if (!is.null(file.prev)) source(file.prev)
-		source(file.exerc)
-		source(file.test)
-		return(sum(ANSWERS))
+		# Executa as precondicoes
+		if(sum(dim(precondi)) > 0) eval(parse(text=precondi), envir=corrEnv);
+
+		# Executa o texto da resposta
+		# try pega erros de sintaxe
+		getError <- try(eval(parse(text=texto), envir=corrEnv));
+		if (class(getError) == "try-error") return (NULL);
+
+		# Executa os testes cadastrados, sequencialmente
+		notaMax <-dim(testes)[1]
+		notas <- rep(FALSE, notaMax)
+		for (i in 1:notaMax) {
+				# A avaliacao pode retornar TRUE, FALSE ou erro
+				# No momento, erro esta sendo tratado como FALSE
+				notas[i] <- try(eval(parse(text=testes[i,1]), envir=corrEnv)) == TRUE;
+		}
+		return(notas);
 }
 
-print(corrige(NULL, file.exerc, file.test))
+# Gera um output formatado em HTML a respeito de um exercicio corrigido
+relatorioNota <- function (numero.aula, numero.exercicio, nota) {
+		# Definicoes iniciais
+		id.exerc <- dbGetQuery(con, 
+							   paste("SELECT id_exercicio FROM exercicio 
+									 WHERE numero_aula=", numero.aula,
+									 " AND numero_exercicio=",numero.exercicio, sep=""));
+		dica <- dbGetQuery (con,
+							paste("SELECT dica FROM teste
+								  WHERE id_exercicio = ", id.exerc,
+								  " ORDER BY ordem ASC ", sep=""));
+		Rel <- paste("<p>Notas para aula ", numero.aula, ", exerc&iacute;cio ", numero.exercicio,":</p>", sep="");
+		if (is.null(nota))
+				return (paste(Rel, "<p><font color='#FF0000'>ERRO!</font> Seu exerc&iacute;cio cont&eacute;m algum
+							  erro de sintaxe! Verifique no R se ele est&aacute; executando.</p>", sep=""));
+		notaMax <-dim(dica)[1]
+		Rel <- paste(Rel, "<p>Acertos: ",sum(nota),"/",notaMax," (<b>", round(100*mean(nota)),"%</b>).</p>", sep="")
+		for (i in 1:notaMax) {
+				if (nota[i])
+						Rel <- paste(Rel, "<br>Parte ",i,": <font color='#00FF00'>SUCESSO</font>", sep="")
+				else    Rel <- paste(Rel, "<br>Parte ",i,": <font color='#8c2618'>ATEN&Ccedil;&Atilde;O!</font><br>&nbsp;&nbsp;",
+									 dica[i,1], sep="");
+		}
 
-# Alternativa: parse+eval:
-eval(parse(text="y<-rnorm(5)"))
+		return(Rel)
+}
+
+# gravarNota recebe
+# nome.aluno (pode ser NULL, no caso de ouvintes)
+# numero.aula, numero.exercicio
+# nota: logical vector contendo o resultado da correcao
+# texto: resposta dada pelo aluno
+# Valor de retorno: char, especificando mensagem de sucesso ou erro 
+# na insercao da nota
+gravarNota <- function (nome.aluno, numero.aula, numero.exercicio, texto, nota = corretoR(numero.aula, numero.exercicio, texto)) {
+		# Definicoes iniciais
+		id.aluno <- dbGetQuery(con, 
+							   paste("SELECT id_aluno FROM aluno 
+									 WHERE nome_aluno ='", nome.aluno,"'", sep=""));
+		id.exerc <- dbGetQuery(con, 
+							   paste("SELECT id_exercicio FROM exercicio 
+									 WHERE numero_aula=", numero.aula,
+									 " AND numero_exercicio=",numero.exercicio, sep=""));
+		prazo <- dbGetQuery(con,
+							paste("SELECT prazo FROM prazo p 
+								  JOIN turma t ON (p.id_turma=t.id_turma)
+								  JOIN aluno a ON (a.id_turma=t.id_turma)
+								  WHERE id_exercicio=", id.exerc, sep=""));
+		Date <- format(Sys.time(), "%F %R");
+
+		# Condicoes para gravar a nota
+		if (sum(dim(id.aluno)) == 0) return ("<p>Aluno n&atilde;o cadastrado! A nota n&atilde;o foi gravada.</p>")
+		if (sum(dim(prazo)) > 0) if (Date > prazo) return ("<p>O prazo para entrega j&aacute; expirou! A nota n&atilde;o foi gravada.</p>")
+		if (is.null(nota)) return ("<p>Imposs&iacute;el gravar nota. C&oacute;digo com erros de sintaxe n&atilde;o.</p>")
+
+		# Faz a insercao da nota. TODO: possibilidade de erros na insercao??
+		res <- dbSendQuery(con,paste("INSERT INTO nota (id_aluno, id_exercicio, data, nota, texto) 
+									 VALUES (",id.aluno,",",id.exerc,",'",Date,"',",round(100*mean(nota)), ",'",texto,"')",sep=""))
+		melhorNota <- dbGetQuery(con,
+								 paste("SELECT max(nota) FROM nota
+									   WHERE id_aluno = ",id.aluno, " AND id_exercicio=",
+									   id.exerc, sep=""));
+		Rel <- paste("<p>Nota cadastrada! Sua melhor nota nesse exerc&iacute;cio &eacute; <b>", melhorNota, 
+					  "%</b>.", sep="")
+		if (sum(dim(prazo)) > 0) Rel <- paste (Rel, "<br>O prazo para enviar novas tentativas &eacute; ", prazo, ".", sep="");
+		return (paste(Rel, "</p>"));
+}
+
+# Recebe o exercicio, corrige, grava a nota e gera um output formatado em HTML
+notaR <- function (nome.aluno, numero.aula, numero.exercicio, texto) {
+		# Definicoes iniciais
+		# Corrige o exercicio
+		nota <- corretoR (numero.aula, numero.exercicio, texto);
+		# Grava a nota no banco:
+		notaGravada <- gravarNota(nome.aluno, numero.aula, numero.exercicio, texto, nota)
+		# Gera o relatorio de notas:
+		Rel <- relatorioNota(numero.aula, numero.exercicio, nota);
+		return(paste(Rel, notaGravada,sep=""))
+
+}
+
+# Exemplos: 
+# Corretor
+# corretoR(3, 2, "
+# dieta<-c(rep(c(\"A\", \"B\", \"C\"),each=6))
+# cor<-c(rep(c(rep(c(\"claro\", \"escuro\"), each=3)), times=3))
+# pesos<-c(0.1, 1.1, 3.7, 1.5, -0.1, 2.0, 5.7, -1.2, -1.5, 0.6, -3.0, -0.3, 3.0, -0.4, 0.6, -0.2, 0.3, 1.5)
+# hamsters<-data.frame(dieta, cor, pesos)
+# ");
+
+# Gravando nota
+# gravarNota ('chalom', 3, 2, "hamsters <- data.frame()");
+# Aluno nao cadastrado
+
+# Relatorio de nota:
+# relatorioNota(3,2,corretoR(3,2,"hamsters <- data.frame()"));
+
+# Geral:
+notaR('chalom', 3,2,"hamsters <- data.frame()")
+notaR('chalom', 3, 2, "hamsters <- data.frae()");
+notaR('xuxa', 3, 2, "hamsters <- data.frame()");
--- tables.sql
+++ tables.sql
@@ -1,26 +1,47 @@
 DROP TABLE IF EXISTS nota;
 DROP TABLE IF EXISTS aluno;
+DROP TABLE IF EXISTS prazo;
+DROP TABLE IF EXISTS turma;
 DROP TABLE IF EXISTS teste;
 DROP TABLE IF EXISTS exercicio;
 
+CREATE TABLE turma (
+	id_turma INT(10) PRIMARY KEY AUTO_INCREMENT,
+	nome_turma VARCHAR(200) NOT NULL
+) ENGINE=INNODB;
+
 CREATE TABLE aluno (
 	id_aluno INT(10) PRIMARY KEY AUTO_INCREMENT,
-	nome_aluno VARCHAR(200) NOT NULL
+	nome_aluno VARCHAR(200) NOT NULL,
+	id_turma INT(10) NOT NULL,
+	INDEX (id_turma),
+	FOREIGN KEY (id_turma) REFERENCES turma (id_turma)
 ) ENGINE=INNODB;
 
 CREATE TABLE exercicio (
 	id_exercicio INT(10) PRIMARY KEY AUTO_INCREMENT,
 	numero_aula INT(4) NOT NULL,
 	numero_exercicio INT(4) NOT NULL,
-	prazo Datetime,
-	precondicoes VARCHAR(200),
+	precondicoes VARCHAR(2000), -- NOTA: Usar ; entre cada statement
 	UNIQUE (numero_aula, numero_exercicio)
+) ENGINE=INNODB;
+
+CREATE TABLE prazo (
+	id_exercicio INT(10) NOT NULL,
+	id_turma INT(10) NOT NULL,
+	prazo Datetime,
+	INDEX (id_exercicio), INDEX(id_turma),
+	FOREIGN KEY (id_exercicio) REFERENCES exercicio (id_exercicio),
+	FOREIGN KEY (id_turma) REFERENCES turma (id_turma),
+	PRIMARY KEY (id_exercicio, id_turma)
 ) ENGINE=INNODB;
 
 CREATE TABLE teste (
 	id_teste INT(10) PRIMARY KEY AUTO_INCREMENT,
 	id_exercicio INT(10) NOT NULL,
 	condicao VARCHAR(200) NOT NULL,
+	ordem INT(4),
+	dica VARCHAR(200),
 	INDEX(id_exercicio),
 	FOREIGN KEY (id_exercicio) REFERENCES exercicio (id_exercicio)
 ) ENGINE=INNODB;
@@ -30,7 +51,7 @@ CREATE TABLE nota (
 	id_aluno INT(10) NOT NULL,
 	id_exercicio INT(10) NOT NULL,
 	data Datetime NOT NULL,
-	nota NUMERIC(2,1) NOT NULL,
+	nota INT(3) NOT NULL,
 	texto VARCHAR(2000) NOT NULL,
 	INDEX (id_exercicio), INDEX (id_aluno),
 	FOREIGN KEY (id_exercicio) REFERENCES exercicio (id_exercicio),
