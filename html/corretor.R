ab73778fa498441fa1d7f357f4f2041175806610
Andre Chalom <andrechalom@gmail.com>
2012-10-22 20:04:30 +0000

    X

--- html/corretor.R
+++ html/corretor.R
@@ -51,9 +51,11 @@ relatorioNota <- function (id.exerc, nota, texto) {
 							paste("SELECT dica FROM teste
 								  WHERE id_exercicio = ", id.exerc,
 								  " ORDER BY ordem ASC ", sep=""));
+		peso <- dbGetQuery (con,
+							paste("SELECT peso FROM teste
+								  WHERE id_exercicio = ", id.exerc,
+								  " ORDER BY ordem ASC ", sep=""));
 		notaMax <-dim(dica)[1]
-
-		
 		Rel <- "";
 		if (sum(nota) != notaMax) { 
 			Rel <- paste(Rel, "<font color='#8c2618'>ATEN&Ccedil;&Atilde;O!</font><br>")
@@ -65,7 +67,7 @@ relatorioNota <- function (id.exerc, nota, texto) {
 		if (is.null(nota)) {
 				Rel <- paste(Rel, "<p><font color='#FF0000'>ERRO!</font> Seu exerc&iacute;cio cont&eacute;m algum
 							  erro de sintaxe! Verifique no R se ele est&aacute; executando.</p>", sep="");
-		} else { Rel <- paste(Rel, "<p>Seu aproveitamento: <b>", round(100*mean(nota)),"%</b>.</p>", sep=""); }
+		} else { Rel <- paste(Rel, "<p>Seu aproveitamento: <b>", round(100*weighted.mean(nota, peso)),"%</b>.</p>", sep=""); }
 		Rel <- paste(Rel, "<p>Sua resposta:<br>", paste(texto, collapse="<br>"),"</p>", sep="");
 		return(Rel)
 }
@@ -93,9 +95,13 @@ gravarNota <- function (nome.aluno, id.exerc, texto, nota = corretoR(id.exerc, t
 		if (sum(dim(prazo)) > 0) if (Date > prazo) return ("<p><font color='#8c2618'>O prazo para entrega j&aacute; expirou!</font> A nota n&atilde;o foi gravada.</p>")
 		if (is.null(nota)) return (NULL);
 
-		# Faz a insercao da nota. TODO: possibilidade de erros na insercao??
+		peso <- dbGetQuery (con,
+							paste("SELECT peso FROM teste
+								  WHERE id_exercicio = ", id.exerc,
+								  " ORDER BY ordem ASC ", sep=""));
+
 		res <- dbSendQuery(con,paste("INSERT INTO nota (id_aluno, id_exercicio, data, nota, texto) 
-									 VALUES (",id.aluno,",",id.exerc,",'",Date,"',",round(100*mean(nota)), ",'",texto,"')",sep=""))
+									 VALUES (",id.aluno,",",id.exerc,",'",Date,"',",round(100*weighted.mean(nota, peso)), ",'",texto,"')",sep=""))
 		melhorNota <- dbGetQuery(con,
 								 paste("SELECT max(nota) FROM nota
 									   WHERE id_aluno = ",id.aluno, " AND id_exercicio=",
