efcc5d8f757c0605f2a0da140274cfa6f6fac983
Andre Chalom <andrechalom@gmail.com>
2013-01-14 22:12:20 +0000

    X

--- html/turmas.php
+++ html/turmas.php
@@ -4,7 +4,19 @@ if (! $user->admin()) {
 	exit;
 }
 ###### Codigo aqui
+$lista_turmas = mysql_query("SELECT id_turma FROM turma");
 ?>
+<h2>Administra&ccedil;&atilde;o de turmas</h2>
+<p>Turmas cadastradas:</p>
+<table><tr><td>Nome</td><td>Alunos</td></tr>
+<?php
+while ($T = mysql_fetch_array($lista_turmas)) {
+	$turma = new Turma($T[0]);
+	echo "<tr><td>".$turma->nome()."</td><td>".$turma->alunos()."</td></tr>";
+}
+?>
+</table>
+
 </div>
 </body>
 </html>
