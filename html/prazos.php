2607d0616c6e5e52ec63e596f384efbe8fa5e184
Andre Chalom <andrechalom@gmail.com>
2013-01-15 18:52:50 +0000

    X

--- html/prazos.php
+++ html/prazos.php
@@ -14,15 +14,16 @@ if (isset($_POST['submit']) AND $_POST['submit'] == "atualiza") {
 		foreach (array_keys($post) AS $key) {
 				if (strpos($key, "ld_")) {
 						$new = substr($key, 4);
+						$ex = substr($key, 6);
 						if ($post[$key] != $post[$new]) {
 								if($post[$key] =='') { // novo
-echo "novo";
+										mysql_query("INSERT INTO prazo (id_exercicio, id_turma, prazo) VALUES ($ex, $turma, '".$post[$new]."')");
 								}
 								elseif($post[$new] == '') { // removido
-echo "remove";
+										mysql_query("DELETE FROM prazo WHERE id_exercicio=$ex AND id_turma=$turma");
 								}
 								else { // atualizar
-echo "Atualiza";
+										mysql_query("UPDATE prazo SET prazo='".$post[$new]."' WHERE id_exercicio=$ex AND id_turma=$turma");
 								}
 						}
 				}
