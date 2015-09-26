4503de4ddbed0a75e6735eb9624cabc5bab2d073
andrechalom <andrechalom@gmail.com>
2015-09-26 01:14:52 -0300

    Update config.php

--- html/config.php
+++ html/config.php
@@ -2,7 +2,7 @@
 define('RSERVE_HOST','localhost');
 $basedir = '/var/www/html/notaR';
 error_reporting(0);
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);
+//ini_set('display_errors', 'On');
+//error_reporting(E_ALL);
 $mysqli = new mysqli("localhost", "notaR", "notarPw", "notaR");
 ?>
