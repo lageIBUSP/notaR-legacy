646c01b612be16305f75b23e839455a4380c314b
Andre Chalom <andrechalom@gmail.com>
2015-09-29 19:45:43 -0300

    Intermediario: movendo grafico p/ webcron e centralizando config

--- html/graficos.php
+++ html/graficos.php
@@ -5,6 +5,31 @@ if (! $USER->admin()) {
 }
 ?>
 <h2>Gr&aacute;ficos (BETA!)</h2>
+
+<?php if (isset($_POST['submit'])) {
+	require_once 'Rserve-php/Connection.php';
+			try{
+				$r = new Rserve_Connection(RSERVE_HOST);
+			} catch (Exception $e) {
+				echo 'Erro interno ao conectar no servidor. Notifique os administradores do sistema!<br>';
+        if (error_reporting() & E_ERROR)
+          echo $e;
+			}
+			try {
+        $text  = 'source("'.$BASEDIR.'/corretor.R");';
+        $text .= 'con <- connect("'.$DBUSER.'","'.$DBPASS.'","'.$DBNAME.'");';
+        $text .= 'PATH <- "'.$BASEDIR.'";';
+        $text .= 'porDow(); porHora();';   
+        $x = $r->evalString($text);   
+			} catch (Exception $e) {
+				echo 'Erro interno ao gerar os graficos.<br>';
+        if (error_reporting() & E_ERROR)
+          echo $e;
+			}
+} 
+?>
+
+<form action="#" method="post"><button name='submit' type='submit' value='submit'>Atualiza!</button></form>
 <p>Atualizados de hora em hora</p>
 <p>Gr&aacute;ficos gerais</p>
 <br><img src="img/porhora.png">
